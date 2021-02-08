8716a27c20efb80358c8e714ffa3adea2069334e
me-anshu7 <me-anshu7@users.noreply.github.com>
2021-02-08 19:56:35 +0530

    Working with NPM Modules.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 console.log(getNotes());
-console.log(validator.isEmail('andrew@example.com'));
-console.log(validator.isEmail('example.com'));
-console.log(validator.isURL('https://mead.io'));
+console.log(chalk.green.bold.inverse('Success!'));
